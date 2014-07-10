e41c5acb29026a05956c3902813aecae44a4c790
Rachel Flores <rachelflores@dev.primergrey.net>
2014-07-10 14:47:29 -0500

    commit some files

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+
+require 'bourbon-compass'
+
+output_style = ":compact"
+line_comments = false
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Git/Sass Training Test</title>
+        <link rel="shortcut icon" href="http://primergrey.com/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+    </head>
+    <body>
+        <section>
+            <img src="/images/pg_logo.png">
+            <a href="http://en.wikipedia.org/wiki/Special:Random" target="_blank">Learn Something.</a>
+        </section>
+    </body>
+</html>
